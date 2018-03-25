edf053ea64da50e935b92940342b57caf6ba7399
GrayWind33 <graywindjy@163.com>
2018-03-26 00:16:55 +0800

    处理拦截器拦截静态资源问题

--- shop/src/main/java/graywind/shop/interceptor/LoginInterceptor.java
+++ shop/src/main/java/graywind/shop/interceptor/LoginInterceptor.java
@@ -1,8 +1,11 @@
 package graywind.shop.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import graywind.shop.bean.SessionData;
@@ -10,6 +13,8 @@ import graywind.shop.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.lang.reflect.Method;
 import java.util.Optional;
 
@@ -19,58 +24,40 @@ import static graywind.shop.interceptor.Constants.USER_CODE_SESSION_KEY;
 
 @Component
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-    private final static String SESSION_KEY_PREFIX = "session:";
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
     @Autowired
     private UserService userSvc;
 
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        String username = (String) request.getSession().getAttribute("username");
-        if (Optional.ofNullable(username).map(String::length).orElse(0) > 0) {
+        // TODO Auto-generated method stub
+        logger.info("------preHandle------");
+        // 获取session
+        HttpSession session = request.getSession(true);
+        // 判断用户ID是否存在，不存在就跳转到登录界面
+        if (session.getAttribute("username") == null) {
+            logger.info("------:跳转到login页面！");
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        } else {
+            session.setAttribute("username", session.getAttribute("username"));
             return true;
         }
+    }
 
-        if (!handler.getClass().isAssignableFrom(HandlerMethod.class)) {
-            return true;
-        }
-        handlerSession(request);
-
-        final HandlerMethod handlerMethod = (HandlerMethod) handler;
-        final Method method = handlerMethod.getMethod();
-        final Class<?> clazz = method.getDeclaringClass();
-        if (clazz.isAnnotationPresent(Auth.class) || method.isAnnotationPresent(Auth.class)) {
-            if (request.getAttribute(USER_CODE_SESSION_KEY) == null) {
-                response.sendRedirect(request.getContextPath() + "/login");
-                return false;
-            } else {
-                return true;
-            }
-        }
-        return true;
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
 
     }
 
-    public void handlerSession(HttpServletRequest request) {
-        String sessionId = request.getHeader(SESSION_KEY);
-        if (Optional.ofNullable(sessionId).map(String::length).orElse(0) == 0) {
-            sessionId = (String) request.getSession().getAttribute(SESSION_KEY);
-        }
-        if (Optional.ofNullable(sessionId).map(String::length).orElse(0) > 0) {
-            SessionData model = userSvc.getSession(SESSION_KEY_PREFIX + sessionId);
-            if (model == null) {
-                return;
-            }
-            request.setAttribute(SESSION_KEY, sessionId);
-            Integer userCode = model.getUserCode();
-            if (userCode != null) {
-                request.setAttribute(USER_CODE_SESSION_KEY, Long.valueOf(userCode));
-            }
-            String mobile = model.getMobileNumber();
-            if (mobile != null) {
-                request.setAttribute(MOBILE_NUMBER_SESSION_KEY, mobile);
-            }
-        }
-        return;
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // TODO Auto-generated method stub
+
     }
 }
--- shop/src/main/java/graywind/shop/interceptor/MvcConfig.java
+++ shop/src/main/java/graywind/shop/interceptor/MvcConfig.java
@@ -1,42 +1,81 @@
 package graywind.shop.interceptor;
 
+import java.nio.charset.Charset;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration  
-@EnableWebMvc  
-@ComponentScan(basePackages = "graywind.shop.controller")  
-@PropertySource(value = "classpath:application.properties",  
-        ignoreResourceNotFound = true,encoding = "UTF-8")
-public class MvcConfig extends WebMvcConfigurerAdapter {
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "graywind.shop.controller")
+@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true, encoding = "UTF-8")
+public class MvcConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
     private static final Logger logger = LoggerFactory.getLogger(MvcConfig.class);
-    
-    @Autowired  
-    LoginInterceptor loginInterceptor; 
-    
-    @Override  
-    public void addInterceptors(InterceptorRegistry registry) {  
-        // 注册监控拦截器  
-        registry.addInterceptor(loginInterceptor)  
-                .addPathPatterns("/**")  
-         .excludePathPatterns("/configuration/ui");  
-  
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    private ApplicationContext applicationContext;
+
+    public MvcConfig() {
+        super();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 拦截规则：除了login，其他都拦截判断
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/user/login",
+                "/register", "/user/register");
+        super.addInterceptors(registry);
     }
     
-    @Override  
-    public void addCorsMappings(CorsRegistry registry) {  
-        registry.addMapping("/**")  
-                .allowedOrigins("*")  
-                .allowedHeaders("*/*")  
-                .allowedMethods("*")  
-                .maxAge(120);  
-    }  
+    //中文乱码处理
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+
 }
